f72e7d3c270ee3005fb42dcb5171f5800ab602b7
Sajad <sajad.tohidi76@gmail.com>
2024-08-03 22:27:16 +0330

    add requirements.txt and model migrations

--- core_apps/accounts/models.py
+++ core_apps/accounts/models.py
@@ -6,7 +6,6 @@ from django.utils.translation import gettext_lazy as _
 class User(AbstractUser):
     phone_number = models.CharField(verbose_name=_('phone number'), max_length=15, unique=True, db_index=True)
 
-
     class Meta:
         verbose_name = _('User')
         verbose_name = _('Users')
